628a2f3113972113740d33dc3d0649de6c235908
Muhammad Junaid Farooq <junaidfarooq.pk@gmail.com>
2025-11-17 01:34:13 +0500

    feat: setup project architecture and build login UI with employee dashboard layout

---
+++ src/components/TasKList/TaskList.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[45%] overflow-x-auto flex items-center justify-start gap-5 w-full flex-nowrap mt-15 py-5'>
+
+      <div className='flex flex-col shrink-0 h-full w-[400px] bg-red-400 p-5 rounded-xl'>
+        <div className="flex justify-between items-start w-full">
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h4 className='text-sm'>17 Nov 2025</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a presentation</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure amet inventore neque explicabo ab?</p>
+      </div>
+
+      <div className='flex flex-col shrink-0 h-full w-[400px] bg-blue-400 p-5 rounded-xl'>
+        <div className="flex justify-between items-start w-full">
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h4 className='text-sm'>17 Nov 2025</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a presentation</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure amet inventore neque explicabo ab?</p>
+      </div>
+
+      <div className='flex flex-col shrink-0 h-full w-[400px] bg-green-400 p-5 rounded-xl'>
+        <div className="flex justify-between items-start w-full">
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h4 className='text-sm'>17 Nov 2025</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a presentation</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure amet inventore neque explicabo ab?</p>
+      </div>
+
+      <div className='flex flex-col shrink-0 h-full w-[400px] bg-yellow-400 p-5 rounded-xl'>
+        <div className="flex justify-between items-start w-full">
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h4 className='text-sm'>17 Nov 2025</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a presentation</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure amet inventore neque explicabo ab?</p>
+      </div>
+
+      <div className='flex flex-col shrink-0 h-full w-[400px] bg-purple-400 p-5 rounded-xl'>
+        <div className="flex justify-between items-start w-full">
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h4 className='text-sm'>17 Nov 2025</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a presentation</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure amet inventore neque explicabo ab?</p>
+      </div>
+
+      <div className='flex flex-col shrink-0 h-full w-[400px] bg-amber-400 p-5 rounded-xl'>
+        <div className="flex justify-between items-start w-full">
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h4 className='text-sm'>17 Nov 2025</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a presentation</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure amet inventore neque explicabo ab?</p>
+      </div>
+
+      <div className='flex flex-col shrink-0 h-full w-[400px] bg-pink-400 p-5 rounded-xl'>
+        <div className="flex justify-between items-start w-full">
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+          <h4 className='text-sm'>17 Nov 2025</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a presentation</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus iure amet inventore neque explicabo ab?</p>
+      </div>
+    </div>
+  )
+}
+
+export default TaskList
---
+++ src/components/other/TaskListNumbers.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const TaskListNumbers = () => {
+  return (
+    <div className="flex mt-10 justify-between screen gap-5">
+      <div className="rounded-xl py-6 px-9 w-[45%] bg-red-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-2xl font-medium">New Task</h3>
+      </div>
+
+      <div className="rounded-xl py-6 px-9 w-[45%] bg-blue-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-2xl font-medium">New Task</h3>
+      </div>
+
+      <div className="rounded-xl py-6 px-9 w-[45%] bg-green-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-2xl font-medium">New Task</h3>
+      </div>
+
+      <div className="rounded-xl py-6 px-9 w-[45%] bg-yellow-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-2xl font-medium">New Task</h3>
+      </div>
+    </div>
+  );
+};
+
+export default TaskListNumbers;
