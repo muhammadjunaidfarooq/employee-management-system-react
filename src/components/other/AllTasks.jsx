cd216ac7ee885e901bd379921b13ec9db180f90f
Muhammad Junaid Farooq <junaidfarooq.pk@gmail.com>
2025-11-18 00:22:41 +0500

    feat: developed admin dashboard interface with modular components and clean UI architecture

---
+++ src/components/other/AllTasks.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const AllTasks = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-70 overflow-auto'>
+      <div className="bg-red-400 py-2 px-4 flex justify-between rounded mb-3">
+        <h2>Junaid</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className="bg-green-400 py-2 px-4 flex justify-between rounded mb-3">
+        <h2>Junaid</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className="bg-blue-400 py-2 px-4 flex justify-between rounded mb-3">
+        <h2>Junaid</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className="bg-yellow-400 py-2 px-4 flex justify-between rounded mb-3">
+        <h2>Junaid</h2>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+
+    </div>
+  )
+}
+
+export default AllTasks
